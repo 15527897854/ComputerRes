9e174bfc049a5eadc43675b208ef22ec1c86eb77
linc212 <1374316846@qq.com>
2017-06-29 13:15:11 +0800

    1

--- ComputerRes/public/views/components/enviro/enviroTableTree.js
+++ ComputerRes/public/views/components/enviro/enviroTableTree.js
@@ -662,7 +662,7 @@ var EnviroTableTree = React.createClass({
         {
             if(this.state.loading)
             {
-                return (<span>Loading...</span>);
+                return (<span><i className="fa fa-spinner fa-spin fa-3x fa-fw" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Loading...</span>);
             }
             if(this.state.err)
             {
